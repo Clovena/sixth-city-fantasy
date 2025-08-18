e86974b225a3b7fc053ada0b1cf5d2fc1e01be74
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-08-18 16:21:33 -0400

    Init footer component.

---
+++ components/footer.js
@@ -0,0 +1,17 @@
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <footer>
+        Copyright &copy; 2025 Zacalytics, LLC.<br />
+        Some rights reserved.<br />
+        Icons and custom fonts by Google Fonts.
+      </footer>
+    `;
+  }
+}
+
+customElements.define('footer-component', Footer);

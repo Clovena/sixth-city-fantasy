78bb5f465912af42a0cf1027e8fc83d1996c3ff9
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-08-18 16:48:31 -0400

    edit footer.

--- components/footer.js
+++ components/footer.js
@@ -6,9 +6,11 @@ class Footer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <footer>
-        Copyright &copy; 2025 Zacalytics, LLC.<br />
+        Copyright &copy; 2025<br />
+        Zacalytics, LLC.<br />
         Some rights reserved.<br />
-        Icons and custom fonts<br />by Google Fonts.
+        Icons and custom fonts<br />
+        by Google Fonts.
       </footer>
     `;
   }
